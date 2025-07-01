44ba1ced175db90d930f104743603f1d13c49a51
Tanvi Agrawal <tanviagrawal9913@gmail.com>
2025-07-01 18:01:03 +0530

    no changes

--- src/Pages/Question/Question.jsx
+++ src/Pages/Question/Question.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Leftsidebar from '../../Component/Leftsidebar/Leftsidebar'
 import Rightsidebar from '../../Component/Rightsidebar/Rightsidebar'
-import Homemainbar from '../../Component/Homemainbar/homemainbar'
+// import Homemainbar from '../../Component/Homemainbar/homemainbar'
+import Homemainbar from '../../Component/Homemainbar/Homemainbar'
 import '../../App.css'
 
 const Question = ({slidein}) => {
